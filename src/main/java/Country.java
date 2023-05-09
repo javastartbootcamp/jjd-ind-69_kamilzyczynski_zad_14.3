fb5b595df4f4ca013dd44960d6522eeb8d4fcaf8
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-05-09 20:23:31 +0200

    Rozwiązanie

---
+++ src/main/java/Country.java
@@ -0,0 +1,40 @@
+public class Country {
+    private String countryCode;
+    private String name;
+    private long population;
+
+    public Country(String countryCode, String name, long population) {
+        this.countryCode = countryCode;
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return name + "(" + countryCode + ") ma " + population + " ludności.";
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -10,7 +14,33 @@ public class Main {
     }
 
     void run(Scanner scanner) {
-        // usupełnij metodę
-    }
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("countries.csv"));
+            Map<String, Country> countryMap = new HashMap<>();
+            String nextLine = null;
+            while ((nextLine = reader.readLine()) != null) {
+                String[] split = nextLine.split(";");
+                long population = Long.parseLong(split[2]);
+                countryMap.put(split[0], new Country(split[0], split[1], population));
+            }
+            reader.close();
 
+            System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
+            scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            scanner.close();
+            Country country = countryMap.get(userInput);
+
+            if (country != null) {
+                System.out.println(country);
+            } else {
+                System.out.println("Kod kraju " + userInput + " nie został znaleziony.");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku countries.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
